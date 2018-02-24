79fdc1a0dc80d745d5a95cb5f8b52a758bb02162
Steven Bender <sbender1990@gmail.com>
2018-02-24 01:21:58 -0600

    work on js more

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,1 +1,28 @@
-//  api key  (AK5yVQ1pQxyK1dlvIofEWgF8vz6bDa0u)(https://api.giphy.com/v1/gifs/random?api_key=AK5yVQ1pQxyK1dlvIofEWgF8vz6bDa0u&tag=&rating=G)
+//  api key  (AK5yVQ1pQxyK1dlvIofEWgF8vz6bDa0u)
+
+var examples = ["cheetah", "Bear", "Monkey", "Snake"]
+
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=api_key=AK5yVQ1pQxyK1dlvIofEWgF8vz6bDa0u";
+
+$.ajax({
+    url: queryURL,
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response);
+    var results = response.data;
+
+    for (var i = 0; i < resilts.length; i++) {
+        var gifDiv = $("<div>");
+        var rating = $("<p>").text("Rating: " + resilts[i].rating);
+        var gifImage = $("<img>");
+
+        gifImage.attr("src", results[i].images.fixed_height.url);
+
+        gifDiv.append(p);
+        gifDiv.append(gifImage);
+
+        $("#animals").prepend(gifDiv);
+    }
+  });
